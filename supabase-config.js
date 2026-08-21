6292fd5efb0538983cae84db8ebfdbd8675e4839
elingne <ielingkk@naver.com>
2026-08-21 14:50:42 +0900

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://fkkgidjqoxtybfzioqyx.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_xTreOotC1z4kBvVASYMfmQ_Dd5nsQsv";
+
+const db = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
